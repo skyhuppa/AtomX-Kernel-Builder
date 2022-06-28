eafafddacb43a2085baee1a74c9ba1f82167e7bc
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2022-06-28 14:22:07 -0700

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -35,7 +35,7 @@
 
 ############################## Setup Kernel ################################
 
-	gut https://github.com/Atom-X-Devs/android_kernel_xiaomi_sm7325 -b kernel.lnx.5.4.r1-rel Kernel
+	gut https://github.com/skyhuppa/android_kernel_xiaomi_sm7325 -b kernel.lnx.5.4.r1-rel Kernel
 
 ############################################################################
 
