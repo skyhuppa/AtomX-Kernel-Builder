2512812b0adda4219b93137b3e7a6e85c9a3ff9d
Adasi_Kaakyire <skyhuppa@hotmail.co.uk>
2022-06-28 15:55:58 -0700

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -12,7 +12,7 @@
 ######################## Setup Telegram API ################################
 
 	@pip -q install telegram-send
-	git clone https://github.com/rahiel/telegram-send $@
+	gut https://github.com/rahiel/telegram-send -b master telegram-send
 	sed -i s/demo1/${BOT_API_KEY}/g telegram-send.conf
 	sed -i s/demo2/${CHAT_ID}/g telegram-send.conf
 	mkdir $HOME/.config
@@ -23,20 +23,20 @@
 ############################## Setup Toolchains ############################
 
 	mkdir toolchains
-	git clone https://github.com/mvaisakh/gcc-arm -b gcc-master toolchains/gcc-arm
-	git clone https://github.com/kdrag0n/proton-clang -b master toolchains/clang
+	gut https://github.com/mvaisakh/gcc-arm -b gcc-master toolchains/gcc-arm
+	gut https://github.com/kdrag0n/proton-clang -b master toolchains/clang
 
 ############################################################################
 
 ############################## Setup AnyKernel #############################
 
-	git clone https://github.com/Atom-X-Devs/AnyKernel3 -b main AnyKernel3
+	gut https://github.com/Atom-X-Devs/AnyKernel3 -b main AnyKernel3
 
 ############################################################################
 
 ############################## Setup Kernel ################################
 
-	git clone https://github.com/skyhuppa/android_kernel_xiaomi_sm7325 -b kernel.lnx.5.4.r1-rel Kernel
+	gut https://github.com/skyhuppa/android_kernel_xiaomi_sm7325 -b kernel.lnx.5.4.r1-rel Kernel
 
 ############################################################################
 
